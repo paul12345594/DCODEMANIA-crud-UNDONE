95fdc903e1ed1178e761c16caa5dbf0cfb1e0304
unknown <dungcapaul6@gmail.com>
2024-12-06 02:06:39 +0800

    ADD NEW USER ---FORM

--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 //URL
 router.get("/", (req, res) => {
     res.render("index", { title: "Home Page" })    // CONNECTED TO HTML 
 });
+
+
+router.get('/add', (req, res) => {
+    res.render('add_users', { title: "Add Users" });    // CONNECTED TO THE add_users.ejs
+})
 
 module.exports = router;
 
