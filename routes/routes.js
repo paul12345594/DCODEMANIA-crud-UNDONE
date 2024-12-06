32611aea19c5018f9805548cb44e94f6dd71abca
unknown <dungcapaul6@gmail.com>
2024-12-07 01:00:02 +0800

    FORM CODING VID 3

--- routes/routes.js
+++ routes/routes.js
@@ -6,7 +6,6 @@ router.get("/", (req, res) => {
     res.render("index", { title: "Home Page" })    // CONNECTED TO HTML 
 });
 
-
 router.get('/add', (req, res) => {
     res.render('add_users', { title: "Add Users" });    // CONNECTED TO THE add_users.ejs
 })
