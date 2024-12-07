976e1f5ab6bc2606004b237439428b0ced7c929e
unknown <dungcapaul6@gmail.com>
2024-12-07 11:58:56 +0800

    upload image

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const User = require ('..models/users');
+const multer = require('multer');
+
+//IMAGE UPLOAD
+var storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './uploads');
+    },
+    filename: function(req, file, cb){
+    cb(null, file.fieldname +"_"+ Date.now +"_"+ file.originalname);
+    },
+});
+//middle ware
+var upload = multer({
+    storage:storage,
+}).single('image');
+
+
+
 
 //URL
 router.get("/", (req, res) => {
