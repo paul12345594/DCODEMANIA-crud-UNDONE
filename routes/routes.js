9f8eaa487d21917f9f25a86a5c3e0fa21478c86e
unknown <dungcapaul6@gmail.com>
2024-12-08 06:32:27 +0800

    FIX PROBLEM

--- routes/routes.js
+++ routes/routes.js
@@ -5,27 +5,60 @@ const multer = require("multer");
 
 //IMAGE UPLOAD
 var storage = multer.diskStorage({
-    destination: function(req, file, cb){
+    destination: function(req, file, cb){           // req, files, cb = arguments 
         cb(null, './uploads');
     },
     filename: function(req, file, cb){
-    cb(null, file.fieldname +"_"+ Date.now +"_"+ file.originalname);
-    },
-});
-//middle ware
+        cb(null, file.fieldname +"_"+ Date.now() +"_"+ file.originalname);
+    }
+})
+
+// LIGHT MIDDLEWARE
 var upload = multer({
-    storage:storage,
-}).single('image');
+    storage: storage,
+}).single("image")
+
+
+// INSERT AN USER INTO DATABASE ROUTE
+router.get("/add", (req, res) => {
+    res.render("add_users", { title: "Add Users" });    // CONNECTED TO THE add_users.ejs
+})
+// Insert an user into the database route
+router.post('/add', upload, async (req, res) => {
+    try {
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            photo: req.file.filename,
+        });
+
+        // Use async/await with save()
+        await user.save();
+
+        req.session.message = {
+            type: 'success',
+            message: 'User added successfully!'
+        };
+
+        res.redirect("/");
+
+    } catch (err) {
+        res.json({ message: err.message, type: 'danger' });
+    }
+});
 
 //URL
 router.get("/", (req, res) => {
     res.render("index", { title: "Home Page" })    // CONNECTED TO HTML 
 });
 
-router.get('/add', (req, res) => {
-    res.render('add_users', { title: "Add Users" });    // CONNECTED TO THE add_users.ejs
-})
+
 
 module.exports = router;
+
+
+
+
 
 
